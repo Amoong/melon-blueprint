b741c5112a41647c298ff02f77bcc9c7c6bb302d
Amoong <moo1323@naver.com>
2023-04-11 15:02:58 +0900

    Update song schema

--- src/models/Song.js
+++ src/models/Song.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 const songSchema = new mongoose.Schema({
   title: { type: String, required: true, trim: true, maxLengh: 30 },
-  artist: String,
+  artist: { type: String, require: true, trim: true, maxLengh: 30 },
+  filename: { type: String, require: true, trim: true, maxLengh: 100 },
   album: String,
   genres: [{ type: String }],
   views: Number,
